3fb9814b4ddebd5969036bad08011df3609438eb
Hussain Bu Subayt <hassunbosbet@gmail.com>
2025-09-07 14:24:25 +0300

    fix code

--- helloworld.java
+++ helloworld.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
+public class helloworld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
 }
