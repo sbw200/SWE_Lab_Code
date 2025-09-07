3b83006ff09d93bbf3c86df85f0939e9310bd278
Saad Bin Waqas <saadbinwaqas2005@gmail.com>
2025-09-07 14:43:09 +0300

    Said hello to Scooby Doo.

--- helloworld.java
+++ helloworld.java
@@ -1,12 +1,5 @@
 public class helloworld {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-=======
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Scooby Doo!");
     }
 }
->>>>>>> 9408cbaa8b28496710673317148691ea8e832ea2
